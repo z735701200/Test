f2abc0dd41eb855fd907dee3e1aad26661c79e22
z735701200 <735701200@qq.com>
2018-08-07 13:51:35 +0800

    修改HelloWorld.java 追加控制台打印

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Test 00001");
-        System.out.println("Test 000002");
-
+        System.out.println("Test 00002");
+        System.out.println("Test 00003");
     }
 }
