9b26b9093ae1b5078e5fceed15a96043738dc418
z735701200 <735701200@qq.com>
2018-08-07 13:58:56 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Test 00002");
         System.out.println("Test 00003");
         System.out.println("Test 00004");
+        System.out.println("Test 00005");
     }
 }
