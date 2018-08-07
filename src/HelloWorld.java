d5de8aa21194a57ac9e04b4c71ff1e7057c43015
z735701200 <735701200@qq.com>
2018-08-07 13:24:13 +0800

    新建测试项目HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Test 00001");
+        System.out.println("Test 000002");
+
+    }
+}
